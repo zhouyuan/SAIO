b88a89799bcf459f3bb2e6d5d69d1fe711f80de3
Yuan Zhou <yuan.zhou@intel.com>
2014-09-01 11:19:19 +0800

    Update default to use 6 disks
    this makes it easier to test 2+2 case

--- setup_saio.sh
+++ setup_saio.sh
@@ -32,14 +32,13 @@ sudo apt-get install -y curl gcc git-core memcached python-configobj /
 sudo useradd -d /home/swift -s /bin/bash -U swift 
 sudo password swift
 #setup xfs for disk
-sudo  apt-get -y install vsftpd 
 
 sudo dd if=/dev/zero of=/srv/swift-disk bs=1024 count=0 seek=1000000
 sudo mkfs.xfs -i size=1024 /srv/swift-disk
 sudo echo "/srv/swift-disk /mnt/sdb1 xfs loop,noatime,nodiratime,nobarrier,logbufs=8 0 0" >> /etc/fstab
 sudo mkdir /mnt/sdb1
 sudo mount /mnt/sdb1
-sudo mkdir /mnt/sdb1/1 /mnt/sdb1/2 /mnt/sdb1/3 /mnt/sdb1/4 /mnt/sdb1/test
+sudo mkdir /mnt/sdb1/1 /mnt/sdb1/2 /mnt/sdb1/3 /mnt/sdb1/4 /mnt/sdb1/5 /mnt/sdb1/6 /mnt/sdb1/test
 sudo chown -R swift:swift /mnt/sdb1/*
 
 if ! [ -e /srv ]
@@ -47,20 +46,20 @@ then
     sudo mkdir /srv 
 fi
 
-for x in 1 2 3 4 
+for x in 1 2 3 4 5 6
 do
   sudo  ln -s /mnt/sdb1/$x /srv/$X
 done
 
 sudo mkdir -p /etc/swift/object-server /etc/swift/container-server /etc/swift/account-server
 
-for x in 1 2 3 4 
+for x in 1 2 3 4 5 6
 do
   sudo mkdir -p /srv/$x/node/sdb$x
 done
 
 sudo mkdir -p /var/run/swift
-sudo  chown -R swift:swift /etc/swift /srv/1/ /srv/2/ /srv/3/ /srv/4/ /var/run/swift
+sudo  chown -R swift:swift /etc/swift /srv/1/ /srv/2/ /srv/3/ /srv/4/ /srv/5/ /srv/6/ /var/run/swift
 
 sudo echo -e "mkdir /var/run/swift \\n chown swift:swift /var/run/swift" >> /etc/rc.local  
 
@@ -76,26 +75,26 @@ if ! [ -e /etc/swift ]
 then
     sudo mkdir /etc/swift
 fi
-chown swift:swift /etc/swift
+sudo chown swift:swift /etc/swift
 
 # copy conf files 
-cp --recursive conf/* /etc/swift/
-cp rsyncd.conf /etc
+sudo cp --recursive conf/* /etc/swift/
+sudo cp rsyncd.conf /etc
 sudo service rsync restart
 
 #some system need easy_install and pip
-apt-get -y install python-setuptools
-easy_install pip
+sudo apt-get -y install python-setuptools
+sudo easy_install pip
 
 #Install swift
-git clone git://github.com/openstack/swift.git
-cd swift ; pip install -r tools/pip-requires
+git clone https://github.com/openstack/swift.git
+cd swift ; sudo pip install -r requirements.txt
 sudo python setup.py install; cd ..
 
 #Install swiftclient
 git clone https://github.com/openstack/python-swiftclient.git
-cd python-swiftclient; pip install -r tools/pip-requires
- sudo python setup.py install;cd ..
+cd python-swiftclient; sudo pip install -r requirements.txt
+sudo python setup.py install; cd ..
 
 #make ring and start server
 cd bin;sh remakerings
